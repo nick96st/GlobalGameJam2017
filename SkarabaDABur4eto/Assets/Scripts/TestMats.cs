87b8a464d7c02a840ea02ac5a7425a804e073a89
nick96st <nikolay.yotov@abv.bg>
2017-01-20 23:43:29 +0000

    vibranium code + prefab

---
+++ SkarabaDABur4eto/Assets/Scripts/TestMats.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TestMats : MonoBehaviour {
+
+  public GameObject VibInstance;
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+    if(Input.GetKeyDown(KeyCode.V)) {
+      VibInstance.GetComponent<Vibranium>().HitObject();
+    }
+	}
+}
