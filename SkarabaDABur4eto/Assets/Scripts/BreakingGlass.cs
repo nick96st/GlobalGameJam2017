d341d6252c60eb5eb6933f2662a6d76291431010
nick96st <nikolay.yotov@abv.bg>
2017-01-21 14:20:02 +0000

    recoded the window to have same structure as others

--- SkarabaDABur4eto/Assets/Scripts/BreakingGlass.cs
+++ SkarabaDABur4eto/Assets/Scripts/BreakingGlass.cs
@@ -1,35 +1,50 @@
-﻿using System.Collections;
+﻿using System;
+using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class BreakingGlass : MonoBehaviour {
+public class BreakingGlass : MonoBehaviour,GameBlockMatI {
 
 
-    public GameObject ParticleGlassPrefab;
-    GameObject ParticleGlass;
-    public GameObject Glass;
-    int count;
-    int max = 25;
+  bool isHit = false;
+  public GameObject ParticleGlassPrefab;
+  GameObject ParticleGlass;
+  public GameObject Glass;
+  //int count;
+  //int max = 25;
 	// Use this for initialization
 	void Start () {
 		
 	}
 	
-	// Update is called once per frame
-	void Update () {
-        if (Input.GetKeyDown(KeyCode.A))
-        {
-            Destroy(Glass);
-            ParticleGlass = Instantiate(ParticleGlassPrefab);
-        }
-        if (Glass==null)
-        {
-            if (count == max)
-            {
-                Destroy(ParticleGlass);
-            }
-            count++;
-        }
+	//// Update is called once per frame
+	//void Update () {
+ //       if (Input.GetKeyDown(KeyCode.A))
+ //       {
+ //           Destroy(Glass);
+ //           ParticleGlass = Instantiate(ParticleGlassPrefab);
+ //       }
+ //       if (Glass==null)
+ //       {
+ //           if (count == max)
+ //           {
+ //               Destroy(ParticleGlass);
+ //           }
+ //           count++;
+ //       }
             
-                }
+ //               }
+
+  IEnumerator GlassBreak() {
+    Destroy(Glass);
+    ParticleGlass = Instantiate(ParticleGlassPrefab);
+    yield return new WaitForSeconds(0.3f);
+    Destroy(ParticleGlass);
+    }
+
+  public void HitObject() {
+    if(!isHit) {
+      StartCoroutine("GlassBreak");
+    }
+  }
 }
---
+++ SkarabaDABur4eto/Assets/Scripts/TestWindowBreak.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TestWindowBreak : MonoBehaviour {
+
+
+  public GameObject glassInstance; 
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		 if(Input.GetKeyDown(KeyCode.G)) {
+      glassInstance.GetComponent<BreakingGlass>().HitObject();
+    }
+	}
+}
