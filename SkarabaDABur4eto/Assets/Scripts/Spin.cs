f9f005f16ac82d30685963af38f78e2784ca099b
Boyan Yotov <boyan.yotov@abv.bg>
2017-01-21 04:26:54 +0200

    Collectible Image and Spin Script (can be used by all)

---
+++ SkarabaDABur4eto/Assets/Scripts/Spin.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Spin : MonoBehaviour {
+
+	[SerializeField]
+	float rotationX;
+	[SerializeField]
+	float rotationY;
+	[SerializeField]
+	float rotationZ;
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		transform.Rotate (new Vector3 (rotationX, rotationY, rotationZ) * Time.deltaTime);
+	}
+}
