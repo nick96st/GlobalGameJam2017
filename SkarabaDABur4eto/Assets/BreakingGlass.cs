1f2999a8fa03b43bd439a1b16614bf5725dcc0c1
AntonM <anton_b_mitkov@abv.bg>
2017-01-21 06:12:28 +0000

    Glass and Particles for Breaking Glass

---
+++ SkarabaDABur4eto/Assets/BreakingGlass.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BreakingGlass : MonoBehaviour {
+
+
+    public GameObject ParticleGlassPrefab;
+    GameObject ParticleGlass;
+    public GameObject Glass;
+    int count;
+    int max = 25;
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        if (Input.GetKeyDown(KeyCode.A))
+        {
+            Destroy(Glass);
+            ParticleGlass = Instantiate(ParticleGlassPrefab);
+        }
+        if (Glass==null)
+        {
+            if (count == max)
+            {
+                Destroy(ParticleGlass);
+            }
+            count++;
+        }
+            
+                }
+}
