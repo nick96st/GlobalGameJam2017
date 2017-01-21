8f4ebbfa8e15befe8fbed065ccbaf527d423ea5d
Boyan Yotov <boyan.yotov@abv.bg>
2017-01-21 05:21:12 +0200

    Statistic added. Collectible added in BoBLevel

---
+++ SkarabaDABur4eto/Assets/Scripts/Statistic.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Statistic : MonoBehaviour {
+
+
+	[SerializeField]
+	int starCounter;
+	[SerializeField]
+	int deathCounter;
+
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+	public void starCollected() {
+		starCounter++;
+	}
+		
+	public void addDeath() {
+		deathCounter++;
+	}
+		
+}
