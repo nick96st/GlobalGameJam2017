23283c3294542ee9a2b90495810485191a6b9213
nick96st <nikolay.yotov@abv.bg>
2017-01-22 07:04:55 +0000

    lock button working and scene for gun..fiexed glass prefab not need to be set

---
+++ SkarabaDABur4eto/Assets/Scripts/UnlockButton.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class UnlockButton : MonoBehaviour {
+
+  [SerializeField]
+  private GameObject[] lockingBlocks;
+  [SerializeField]
+  Sprite PressedButton;
+
+  bool hasBeenPressed = false;
+
+  void OnTriggerEnter2D(Collider2D other) {
+    if (!hasBeenPressed && other.tag != "Finish") {
+      StartCoroutine("DestroyBlocks");
+    }
+  } 
+
+  IEnumerator DestroyBlocks() {
+    // sets as pressed
+    this.gameObject.GetComponent<SpriteRenderer>().sprite = PressedButton;
+    hasBeenPressed = true;
+
+    foreach( GameObject block in lockingBlocks) {
+      Destroy(block);
+      yield return new WaitForSeconds(0.25f);
+    }
+    yield return null;
+  }
+
+}
