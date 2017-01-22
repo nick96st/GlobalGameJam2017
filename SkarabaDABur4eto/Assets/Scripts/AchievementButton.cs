4fdc1b99b612bde4d98bdbecb3e834cf9cf6181e
Boyan Yotov <boyan.yotov@abv.bg>
2017-01-22 02:08:21 +0200

    Demo Achievements. HotKey (I)

---
+++ SkarabaDABur4eto/Assets/Scripts/AchievementButton.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class AchievementButton : MonoBehaviour {
+	
+
+	public GameObject achievementList;
+	[SerializeField]
+	Sprite neutral, highlight;
+
+	private Image imageSprite;
+	void Awake() {
+		imageSprite = this.gameObject.GetComponent<Image> ();
+	}
+	// Use this for initialization
+	void Start () {
+
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+	public void SetClicked() {
+		imageSprite.sprite = highlight;
+		achievementList.SetActive (true);
+	}
+
+	public void UnSetClicked() {
+		imageSprite.sprite = neutral;
+		achievementList.SetActive (false);
+	}
+}
