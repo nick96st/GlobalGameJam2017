f714c5b5e78f07d4181c28f08f1dd8248d0738d0
Georgi Mirazchiyski <Georgi Mirazchiyski>
2017-01-22 01:07:36 +0000

    Shooting anim/mechan

--- SkarabaDABur4eto/Assets/Scripts/RaycastWaves.cs
+++ SkarabaDABur4eto/Assets/Scripts/RaycastWaves.cs
@@ -20,6 +20,8 @@ public class RaycastWaves : MonoBehaviour
     [SerializeField]
     Animator playerController;
 
+    bool isShooting;
+
     void Start ()
     {
         if (GetComponent<Transform>().FindChild("FirePoint") != null)
@@ -30,66 +32,90 @@ public class RaycastWaves : MonoBehaviour
 	
 	void Update ()
     {
-		if(fireRate == 0)
+        if (fireRate == 0)
         {
-            if(Input.GetMouseButtonDown(0))
+            if (Input.GetMouseButtonDown(0))
             {
                 Shoot();
+            }
+            else
+            {
+                isShooting = false;
+                playerController.SetFloat("Speed", 0.0f);
             }
         }
         else
         {
-            if(Input.GetMouseButtonDown(0))
+            if (Input.GetMouseButtonDown(0))
             {
                 timeToFire = Time.time + 1 / fireRate;
                 Shoot();
             }
+            else
+            {
+                isShooting = false;
+                playerController.SetFloat("Speed", 0.0f);
+            }
         }
-	}
+
+        playerController.SetBool("isShooting", isShooting);
+    }
 
     private void Shoot()
     {
+        isShooting = true;
+        // playerController.SetBool("isShooting", isShooting);
+
         Vector2 mousePosition = new Vector2(Camera.main.ScreenToWorldPoint(Input.mousePosition).x,
                                             Camera.main.ScreenToWorldPoint(Input.mousePosition).y);
 
         Vector2 mousePos = Input.mousePosition;
 
-        Vector2 firePointPosition =firePoint.position;
+        Vector2 firePointPosition = firePoint.position;
         Quaternion firePointRotation = firePoint.rotation;
 
         RaycastHit2D hit = Physics2D.Raycast(firePointPosition, mousePosition - firePointPosition, 100.0f, whatToHit);
-        //Debug.DrawLine(firePointPosition, (mousePosition - firePointPosition) * 100, Color.cyan);
 
-        float x = mousePos.x - firePointPosition.x;
-        float y = mousePos.y - firePointPosition.y;
+        float x = mousePosition.x - firePointPosition.x;
+        float y = mousePosition.y - firePointPosition.y;
 
         float angle = Mathf.Atan2(y, x) * Mathf.Rad2Deg;
 
+        // checker so it does shoot at himself
+
+        if(angle>=-90 && angle <=90)
+        {
+            if (this.GetComponent<Transform>().localRotation.y != 0)
+            {
+                isShooting = false;
+                return;
+            }   
+        }
+        else
+        {
+            if (this.GetComponent<Transform>().localRotation.y == 0)
+            {
+                isShooting = false;
+                return;
+            }
+        }
+        // float angleSin = Mathf.Sin(angle);
+
         Quaternion rotation = Quaternion.Euler(new Vector3(0, 0, angle));
 
-        if (hit.collider != null)
-        {
-            Debug.DrawLine(firePointPosition, hit.point, Color.red);
-            Debug.Log("You hit " + hit.collider.name);
-        }
-
-        // float shootAngle = Mathf
-        Debug.Log(angle);
-        playerController.SetFloat("ShootingARM", hei)
         GameObject wep;
         if (hit.collider != null && !hit.collider.CompareTag("Player"))
         {
             for (int i = 0; i < 3; i++)
             {
-                wep = Instantiate(weaponToThrow, firePointPosition, Quaternion.identity);
+                wep = Instantiate(weaponToThrow, firePointPosition, rotation);
                 wep.GetComponent<Rigidbody2D>().gravityScale = 0.0f;
 
                 Vector2 direction = mousePosition - firePointPosition;
                 wep.GetComponent<Rigidbody2D>().velocity = direction * fireRate;
             }
         }
-        /*
-        else if ((angle < 130 && angle > -170))
+        else if ((angle < 90 && angle > -90))
         {
             for (int i = 0; i < 3; i++)
             {
@@ -100,6 +126,17 @@ public class RaycastWaves : MonoBehaviour
                 wep.GetComponent<Rigidbody2D>().velocity = direction * fireRate;
             }
         }
-        */
+
+        if (isShooting)
+        {
+            playerController.SetFloat("Angle", angle);
+        }
+
+        return;
     }
+    /*
+    IEnumerator ShootAnim()
+    {
+        isShooting = true;
+    }*/
 }
